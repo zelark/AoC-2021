c389ed2e18fa9aa354574b1039bec2ea5d9172e2
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2021-12-04 09:38:37 +0300

    Refact day 04

--- src/zelark/aoc_2021/day_04.clj
+++ src/zelark/aoc_2021/day_04.clj
@@ -7,55 +7,48 @@
 
 (def input (aoc/get-input 2021 04))
 
-(defn parse-numbers [xs]
-  (->> (str/split xs #",|\s")
-       (remove str/blank?)
-       (mapv parse-long)))
+(defn parse-numbers [s]
+  (as-> (str/trim s) $$
+    (str/split $$ #",|\s+")
+    (mapv parse-long $$)))
 
 (defn parse-board [xs]
-  (let [board (mapv parse-numbers (next xs))]
-    {:original board
-     :rotated  (apply mapv vector board)}))
+  (mapv parse-numbers (next xs)))
 
 (defn parse-input [input]
   (let [[numbers & boards] (str/split-lines input)]
     {:numbers (parse-numbers numbers)
      :boards  (set (map parse-board (partition 6 boards)))}))
 
-(defn winner [drawn-numbers {:keys [original rotated] :as board}]
-  (when (or (some #(every? drawn-numbers %) original)
-            (some #(every? drawn-numbers %) rotated))
-    board))
+(defn winner? [drawn-numbers board]
+  (or (some #(every? drawn-numbers %) board)
+      (some #(every? drawn-numbers %)
+            (apply mapv vector board))
+      false))
 
-(defn score [drawn-numbers n {board :original}]
+(defn score [drawn-numbers n board]
   (->> (reduce into board)
        (remove drawn-numbers)
        (reduce +)
        (* n)))
 
+(defn solve [input]
+  (let [{:keys [numbers boards]} (parse-input input)]
+    (-> (reduce (fn [[drawn-numbers left-boards winning-boards] n]
+                  (let [drawn-numbers' (conj drawn-numbers n)
+                        winners (filter #(winner? drawn-numbers' %) left-boards)]
+                    (if (seq winners)
+                      [drawn-numbers'
+                       (reduce disj left-boards winners)
+                       (into winning-boards
+                             (map #(score drawn-numbers' n %) winners))]
+                      [drawn-numbers' left-boards winning-boards])))
+                [(set (take 4 numbers)) boards []]
+                (drop 4 numbers))
+        (peek))))
+
 ;; part 1
-(let [{:keys [numbers boards]} (parse-input input)]
-  (reduce (fn [nums n]
-            (let [nums' (conj nums n)
-                  board (some #(winner nums' %) boards)]
-              (if board
-                (reduced (score nums' n board))
-                nums')))
-          (set (take 4 numbers))
-          (drop 4 numbers))) ; 35670
+(first (solve input)) ; 35670
 
 ;; part 2
-(let [{:keys [numbers boards]} (parse-input input)]
-  (-> (reduce (fn [[drawn-numbers left-boards winning-boards] n]
-                (let [drawn-numbers' (conj drawn-numbers n)
-                      winners (filter #(winner drawn-numbers' %) left-boards)]
-                  (if (seq winners)
-                    [drawn-numbers'
-                     (reduce disj left-boards winners)
-                     (into winning-boards
-                           (map #(score drawn-numbers' n %) winners))]
-                    [drawn-numbers' left-boards winning-boards])))
-              [(set (take 4 numbers)) boards []]
-              (drop 4 numbers))
-      (peek)
-      (peek))) ; 22704
+(peek (solve input)) ; 22704
