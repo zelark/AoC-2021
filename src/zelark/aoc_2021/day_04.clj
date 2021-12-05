1fcfdb2159232ddd12d1587efc8ee40abb75a03a
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2021-12-05 07:49:34 +0300

    Polish day 04

--- src/zelark/aoc_2021/day_04.clj
+++ src/zelark/aoc_2021/day_04.clj
@@ -8,47 +8,45 @@
 (def input (aoc/get-input 2021 04))
 
 (defn parse-numbers [s]
-  (as-> (str/trim s) $$
-    (str/split $$ #",|\s+")
-    (mapv parse-long $$)))
+  (->> (re-seq #"\d+" s)
+       (map parse-long)))
 
-(defn parse-board [xs]
+ (defn parse-board [xs]
   (mapv parse-numbers (next xs)))
-
+  
 (defn parse-input [input]
   (let [[numbers & boards] (str/split-lines input)]
     {:numbers (parse-numbers numbers)
      :boards  (set (map parse-board (partition 6 boards)))}))
 
 (defn winner? [drawn-numbers board]
-  (or (some #(every? drawn-numbers %) board)
-      (some #(every? drawn-numbers %)
-            (apply mapv vector board))
-      false))
+  (->> (concat board (apply mapv vector board))
+       (some #(every? drawn-numbers %))
+       (boolean)))
 
-(defn score [drawn-numbers n board]
-  (->> (reduce into board)
+(defn score [drawn-numbers last-drown-number board]
+  (->> (flatten board)
        (remove drawn-numbers)
        (reduce +)
-       (* n)))
+       (* last-drown-number)))
 
 (defn solve [input]
   (let [{:keys [numbers boards]} (parse-input input)]
-    (-> (reduce (fn [[drawn-numbers left-boards winning-boards] n]
+    (-> (reduce (fn [[drawn-numbers boards scores] n]
                   (let [drawn-numbers' (conj drawn-numbers n)
-                        winners (filter #(winner? drawn-numbers' %) left-boards)]
+                        winners (filter #(winner? drawn-numbers' %) boards)]
                     (if (seq winners)
                       [drawn-numbers'
-                       (reduce disj left-boards winners)
-                       (into winning-boards
+                       (reduce disj boards winners)
+                       (into scores
                              (map #(score drawn-numbers' n %) winners))]
-                      [drawn-numbers' left-boards winning-boards])))
+                      [drawn-numbers' boards scores])))
                 [(set (take 4 numbers)) boards []]
                 (drop 4 numbers))
         (peek))))
 
 ;; part 1
 (first (solve input)) ; 35670
-
+       
 ;; part 2
 (peek (solve input)) ; 22704
