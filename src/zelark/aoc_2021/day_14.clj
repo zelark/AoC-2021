350a17fe664e289baf992a3e1f2674ff5817449c
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2021-12-14 10:57:18 +0300

    Refact day 14

--- src/zelark/aoc_2021/day_14.clj
+++ src/zelark/aoc_2021/day_14.clj
@@ -20,21 +20,22 @@
                  (-> m
                      (update [a c] (fnil + 0) v)
                      (update [c b] (fnil + 0) v))
-                 m)) {} pairs))
+                 m))
+             {}
+             pairs))
 
 (defn solve [input n]
-  (let [{:keys [template rules]} (parse-input input)
-        freqs (->> (partition 2 1 template)
-                   (frequencies))
-        freqs' (->> (iterate #(step % rules) freqs)
-                    (drop n)
-                    (first)
-                    (reduce-kv (fn [m [a b] v]
-                                 (-> m
-                                     (update a (fnil + 0) v)
-                                     (update b (fnil + 0) v))) {}))]
-    (inc (- (quot (apply max (vals freqs')) 2)
-            (quot (apply min (vals freqs')) 2)))))
+  (let [{:keys [template rules]} (parse-input input)]
+    (->> (partition 2 1 template)
+         (frequencies)
+         (iterate #(step % rules))
+         (drop n)
+         (first)
+         (reduce-kv (fn [m [a] v] (update m a (fnil + 0) v))
+                    {(last template) 1})
+         (vals)
+         (apply (juxt max min))
+         (apply -))))
 
 ;; part 1
 (solve input 10) ; 2899
